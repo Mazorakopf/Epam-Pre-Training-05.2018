67303f2b7e84ec71cfa20010eda0c82e4fb5b431
Mazorakopf <slawa.kleshenko@yandex.ru>
2018-06-05 17:18:50 +0300

    Add exception

--- src/by/epampretraining/kleshchenko/task04/NaturalNum.java
+++ src/by/epampretraining/kleshchenko/task04/NaturalNum.java
@@ -3,48 +3,55 @@ package by.epampretraining.kleshchenko.task04;
 public class NaturalNum {
 
 	public static final int RANK = 10;
-	public static final int ITS_NOT_NATURAL = -1;
+	public static final String ITS_NOT_NATURAL = "It's not natural number";
 	public static final int FIRST_PERFECT_NUM = 6;
 
-	public static boolean isPerfect(int num) {
+	public static boolean isPerfect(int num) throws NaturalNumException {
+
+		int sum = 1;
+
+		if (num < 0) {
+			throw new NaturalNumException(ITS_NOT_NATURAL);
+		}
 
 		if (num >= FIRST_PERFECT_NUM) {
-			int sum = 1;
 			for (int div = 2; div < num; div++) {
 				if (num % div == 0) {
 					sum += div;
 				}
 			}
-			if (sum == num) {
-				return true;
-			}
 		}
 
-		return false;
+		return (sum == num) ? true : false;
 	}
 
-	public static int maxDigit(int num) {
+	public static int maxDigit(int num) throws NaturalNumException {
 
 		int maxDigit = num % RANK;
 
-		if (num > 0) {
-			int tempNum = num;
-			for (int i = 1; i < getCountsOfDigits(num); i++) {
-				int nextDigit = (tempNum /= RANK) % RANK;
-				if (maxDigit < nextDigit) {
-					maxDigit = nextDigit;
-				}
-			}
-			return maxDigit;
+		if (num < 0) {
+			throw new NaturalNumException(ITS_NOT_NATURAL);
 		}
-		return ITS_NOT_NATURAL;
+
+		int tempNum = num;
+		for (int i = 1; i < getCountsOfDigits(num); i++) {
+			int nextDigit = (tempNum /= RANK) % RANK;
+			if (maxDigit < nextDigit) {
+				maxDigit = nextDigit;
+			}
+		}
+		return maxDigit;
 	}
 
-	public static int reverseNum(int num) {
+	public static int reverseNum(int num) throws NaturalNumException {
 
-		int lastDigit= num % RANK;
+		int lastDigit = num % RANK;
 		int newNum = lastDigit;
 		int tempNum = num;
+
+		if (num < 0) {
+			throw new NaturalNumException(ITS_NOT_NATURAL);
+		}
 
 		for (int i = 1; i < getCountsOfDigits(num); i++) {
 			int nextDigit = (tempNum /= RANK) % RANK;
@@ -54,23 +61,34 @@ public class NaturalNum {
 		return newNum;
 	}
 
-	public static boolean isPolindrom(int num) {
+	public static boolean isPolindrom(int num) throws NaturalNumException {
+
+		if (num < 0) {
+			throw new NaturalNumException(ITS_NOT_NATURAL);
+		}
 
 		return num == reverseNum(num);
 	}
 
-	public static boolean isPrime(int num) {
+	public static boolean isPrime(int num) throws NaturalNumException {
+
+		if (num < 0) {
+			throw new NaturalNumException(ITS_NOT_NATURAL);
+		}
 
 		for (int div = 2; div < num; div++) {
 			if (num % div == 0) {
 				return false;
 			}
 		}
-
 		return true;
 	}
 
-	public static int numOfDifDigit(int num) {
+	public static int numOfDifDigit(int num) throws NaturalNumException {
+		
+		if (num < 0) {
+			throw new NaturalNumException(ITS_NOT_NATURAL);
+		}
 
 		int count = 0;
 		for (int i = 0; i < RANK; i++) {
@@ -86,7 +104,11 @@ public class NaturalNum {
 		return count;
 	}
 
-	public static String primeDivid(int num) {
+	public static String primeDivid(int num) throws NaturalNumException {
+		
+		if (num < 0) {
+			throw new NaturalNumException(ITS_NOT_NATURAL);
+		}
 
 		StringBuilder str = new StringBuilder();
 
@@ -98,15 +120,30 @@ public class NaturalNum {
 		return str.toString();
 	}
 
-	public static int gcd(int a, int b) {
+	public static int gcd(int a, int b) throws NaturalNumException {
+		
+		if (a < 0 || b < 0) {
+			throw new NaturalNumException(ITS_NOT_NATURAL);
+		}
+
 		return b == 0 ? Math.abs(a) : gcd(b, a % b);
 	}
 
-	public static int lcm(int a, int b) {
+	public static int lcm(int a, int b) throws NaturalNumException {
+		
+		if (a < 0 || b < 0) {
+			throw new NaturalNumException(ITS_NOT_NATURAL);
+		}
+		
 		return a / gcd(a, b) * b;
 	}
 
-	public static int getCountsOfDigits(int num) {
+	public static int getCountsOfDigits(int num) throws NaturalNumException {
+		
+		if (num < 0) {
+			throw new NaturalNumException(ITS_NOT_NATURAL);
+		}
+		
 		return (num == 0) ? 1 : (int) Math.ceil(Math.log10(Math.abs(num) + 0.5));
 	}
 }
--- src/by/epampretraining/kleshchenko/task04/Task04.java
+++ src/by/epampretraining/kleshchenko/task04/Task04.java
@@ -2,19 +2,22 @@ package by.epampretraining.kleshchenko.task04;
 
 public class Task04 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 
-		System.out.println(HeadsOrTails.countHeads(10));
-		System.out.println(HeadsOrTails.countTails(10));
-		System.out.println(NaturalNum.isPerfect(28));
-		System.out.println(NaturalNum.maxDigit(8765432));
-		System.out.println(NaturalNum.isPrime(13));
-		System.out.println(NaturalNum.numOfDifDigit(339634));
-		System.out.println(NaturalNum.gcd(12, 24));
-		System.out.println(NaturalNum.lcm(5, 12));
-		System.out.println(NaturalNum.reverseNum(123445));
-		System.out.println(NaturalNum.isPolindrom(1225221));
-		System.out.println(NaturalNum.primeDivid(13));
+		try {
+			System.out.println(HeadsOrTails.countHeads(10));
+			System.out.println(HeadsOrTails.countTails(10));
+			System.out.println(NaturalNum.isPerfect(6));
+			System.out.println(NaturalNum.isPrime(4));
+			System.out.println(NaturalNum.numOfDifDigit(339634));
+			System.out.println(NaturalNum.gcd(12, 24));
+			System.out.println(NaturalNum.lcm(5, 12));
+			System.out.println(NaturalNum.reverseNum(123445));
+			System.out.println(NaturalNum.isPolindrom(1225221));
+			System.out.println(NaturalNum.primeDivid(13));
+		}catch(NaturalNumException e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
-
 }
